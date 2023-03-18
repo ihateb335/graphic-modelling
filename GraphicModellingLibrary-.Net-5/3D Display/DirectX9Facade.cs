632f67e98ac6977bbecf7a0715082067d9923363
MrZahn <mykhailo.zhar@stud.onu.edu.ua>
2023-03-18 13:44:41 +0200

    Transferred library to .Net 5.0

---
+++ GraphicModellingLibrary-.Net-5/3D Display/DirectX9Facade.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+namespace GraphicModellingLibrary._3D_Display
+{
+    public class DirectX9Facade
+    {
+        private Device d3d;
+        private Control control;
+
+        public bool BuildUp()
+        {
+            try
+            {
+                // Устанавливаем режим отображения трехмерной графики
+                PresentParameters d3dpp = new PresentParameters();
+                d3dpp.BackBufferCount = 1;
+                d3dpp.SwapEffect = SwapEffect.Discard;
+                d3dpp.Windowed = true; // Выводим графику в окно
+                d3dpp.MultiSample = MultiSampleType.None; // Выключаем антиалиасинг
+                d3dpp.EnableAutoDepthStencil = true; // Разрешаем создание z-буфера
+
+                d3dpp.AutoDepthStencilFormat = DepthFormat.D16; // Z-буфер в 16 бит
+                d3d = new Device(0, // D3D_ADAPTER_DEFAULT - видеоадаптер по
+                                    // умолчанию
+                DeviceType.Hardware, // Тип устройства - аппаратный ускоритель
+                control, // Окно для вывода графики
+                CreateFlags.SoftwareVertexProcessing, // Геометрию обрабатывает CPU
+                d3dpp);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Ошибка инициализации");
+                return true; // Закрываем окно
+            }
+
+            return false;
+        }
+    }
+}
---
+++ GraphicModellingLibrary-.Net-5/CinematicPair.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Numerics;
+
+namespace GraphicModellingLibrary
+{
+    public class CinematicPair
+    {
+        private static readonly double Alpha = Math.PI / 2.0;
+        public Vector3 Link { get; set; }
+        public CinematicPair? LinkHolder { get; set; } = null;
+        public double Fi { get; set; } = Math.PI / 2.0;
+        public double K { get; set; } = 1;
+
+        /// <summary>
+        /// Rotation matrix - M in notation
+        /// </summary>
+        public double[,] M => new double[3,3] {
+                 { Math.Cos(Fi),  Math.Sin(Fi), 0.0 },
+                 {-Math.Sin(Fi),  Math.Cos(Fi), 0.0 },
+                 { 0.0,           0.0,          1.0 },
+        };
+
+        /// <summary>
+        /// Transition matrix - A 
+        /// </summary>
+        public double[,] A => new double[3,3] {
+                 { Math.Cos(K * Alpha), 0.0, -Math.Sin(K * Alpha), },
+                 { Math.Sin(K * Alpha), 0.0, Math.Cos(K * Alpha), },
+                 { 0.0,          1.0, 0.0           },
+        };
+
+        public Vector3 AbsLink
+        {
+            get {
+                if (LinkHolder == null) return Link;
+                var current_holder = this;
+                var result_vector = Link;
+                while(current_holder.LinkHolder != null)
+                {
+                   result_vector = result_vector.RightMultiply(current_holder.M).RightMultiply(A);
+                   current_holder = current_holder.LinkHolder;
+                }
+                return LinkHolder.AbsLink + result_vector;
+            }
+        }
+
+    }
+}
